f23491b485f26b452d597ab0e6b348d7ea35c7a9
mersedee <mersede.rahbar@gmail.com>
2021-10-21 11:33:37 +0330

    add genres

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,3 +1,31 @@
 export type media = 'all'| 'movie'| 'tv'| 'person';
 
 export type timeWindow = 'day' | 'week';
+
+export type Genre = {
+    id: number,
+    name: string,
+}
+
+export const GENRERS = [
+  { id: 0, name: 'All' },
+  { id: 28, name: 'Action' },
+  { id: 18, name: 'Drama' },
+  { id: 16, name: 'Animation' },
+  { id: 53, name: 'Thriller' },
+  { id: 12, name: 'Adventure' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' },
+];
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,63 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 import Header from 'components/header';
 import { GlobalContext } from 'context/GlobalState';
 import { getMovieUrl } from 'api';
+import { Genre, GENRERS } from 'api/types';
+import { Movie } from 'models';
 
 import styles from './styles.module.scss';
 
 const HomePage = () => {
+  const [trendingMovie, setTrendingMovie] = useState<Movie>({} as Movie);
+  const [genres, setGenres] = useState<Genre[]>([]);
   const data = useContext(GlobalContext);
   const { trendingMovies } = data.state;
-  const headerImg = getMovieUrl(trendingMovies[0]?.backdrop_path, 800, 1920);
-  const gradiantStyle = 'linear-gradient(to bottom right, rgba(8.24%, 10.98%, 11.76%, 0.90), rgba(8.24%, 10.98%, 11.76%, 0.74))';
+  const gradiantStyle = 'linear-gradient(to bottom right, rgba(8.24%, 10.98%, 11.76%, 0.70), rgba(8.24%, 10.98%, 11.76%, 0.54))';
+
+  const findGenres = (movie: Movie) => {
+    const formatted:Genre[] = [];
+
+    GENRERS.forEach((genre: Genre) => {
+      movie.genre_ids.forEach((id) => {
+        if (parseInt(id, 10) === genre.id) {
+          formatted.push(genre);
+        }
+      });
+    });
+
+    setGenres(formatted);
+  };
+
+  useEffect(() => {
+    setTrendingMovie(trendingMovies[0]);
+    if (trendingMovies.length > 0) {
+      findGenres(trendingMovies[0]);
+    }
+  }, [trendingMovies]);
 
   return (
     <div className="container-fluid px-0">
       <div
         className={styles.header}
-        style={{ backgroundImage: `${gradiantStyle}, url("${headerImg}")` }}
+        style={{
+          backgroundImage: `${gradiantStyle},
+         url("${getMovieUrl(trendingMovie?.backdrop_path, 800, 1920)}")`,
+        }}
       >
         <div className="layout">
           <Header />
-          <div className={styles['header-info']} />
+          <div className={classNames(styles['header-info'], 'layout')}>
+            <h1 className={styles['header-info-title']}>{trendingMovie?.title?.toUpperCase()}</h1>
+            <div className="d-flex">
+              {genres.map((genre: Genre) => (
+                <div className={styles['header-info-detail']} key={genre.id}>
+                  {genre.name}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
