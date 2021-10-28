e9549cf1d21c2c9faed6b86984fba91b4e5072c2
mersedee <mersede.rahbar@gmail.com>
2021-10-28 17:02:54 +0330

    add result box

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -7,19 +7,29 @@ const Searchbar = () => {
   const [active, setActive] = useState(false);
 
   return (
-    <div className={classNames(styles.search, active && styles.active)}>
-      <input
-        type="text"
-        className={styles.input}
-        placeholder={active ? 'Search for a Movie' : ''}
-      />
-      <button
-        type="button"
-        className={styles.btn}
-        onClick={() => setActive(!active)}
-      >
-        <i className="fa fa-search" aria-hidden="true" />
-      </button>
+    <div className="position-relative">
+      <div className={classNames(styles.search, active && styles.active)}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder={active ? 'Search for a Movie' : ''}
+        />
+        <button
+          type="button"
+          className={styles.btn}
+          onClick={() => setActive(!active)}
+        >
+          <i className="fa fa-search" aria-hidden="true" />
+        </button>
+      </div>
+      {active
+        && (
+        <div className={styles.result}>
+          <div className={styles['result-item']}>item1</div>
+          <div className={styles['result-item']}>item1</div>
+          <div className={styles['result-item']}>item1</div>
+        </div>
+        )}
     </div>
   );
 };
